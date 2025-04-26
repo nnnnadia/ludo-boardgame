18d1f77a23101caef92c854fd02a92bbc8cbb097
nnnnadia <tristaonadia@gmail.com>
2025-04-25 21:15:26 -0300

    Add BoardCell component for game board cells
    - Created a new BoardCell.vue component to represent individual cells on the game board.
    - Implemented props for row, column, and custom classes to manage cell styling.
    - Added logic to determine cell background based on predefined constants for different bases.
    - Updated GameBoard.vue to utilize the new BoardCell component, enhancing modularity and readability.

---
+++ src/components/BoardCell.vue
@@ -0,0 +1,77 @@
+<script setup>
+import {
+  RED_START,
+  BLUE_START,
+  YELLOW_START,
+  GREEN_START
+} from '../constants/base'
+
+const props = defineProps({
+  row: {
+    type: Number,
+    required: true
+  },
+  col: {
+    type: Number,
+    required: true
+  },
+  classes: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+/**
+ * Checks if a given row and column match a single coordinate
+ * @param {[number, number]} coordinate - A single [row, col] coordinate
+ * @param {number} row - The row to check
+ * @param {number} col - The column to check
+ * @returns {boolean} True if the row and column match the coordinate
+ */
+const onSquare = (coordinate, row, col) => {
+  return coordinate[0] === row && coordinate[1] === col
+}
+</script>
+
+<template>
+  <div
+    class="cell"
+    :class="{
+      ...classes,
+      'red-start': onSquare(RED_START, row, col),
+      'blue-start': onSquare(BLUE_START, row, col),
+      'yellow-start': onSquare(YELLOW_START, row, col),
+      'green-start': onSquare(GREEN_START, row, col),
+    }"
+  >
+    {{ row + ', ' + col }}
+  </div>
+</template>
+
+<style scoped>
+.cell {
+  border: solid black 1px;
+  color: black;
+  font-size: xx-small;
+  width: 40px;
+  height: 40px;
+}
+
+.red-start {
+  background-image: url(../assets/star.svg);
+  background-size: cover;
+  background-position: center;
+}
+
+.blue-start {
+  background: blue;
+}
+
+.yellow-start {
+  background: yellow;
+}
+
+.green-start {
+  background: green;
+}
+</style>
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,93 +1,76 @@
 <script setup>
 import { findPath } from '../mappings/totalPath'
 import { ref } from 'vue'
-import { ZONE_SIZE, BOARD_SIZE } from '../constants/board'
+import { BOARD_SIZE } from '../constants/board'
+import {
+  RED_BASE,
+  BLUE_BASE,
+  YELLOW_BASE,
+  GREEN_BASE
+} from '../constants/base'
+import BoardCell from './BoardCell.vue'
+
+const redBase = ref(RED_BASE)
+const blueBase = ref(BLUE_BASE)
+const yellowBase = ref(YELLOW_BASE)
+const greenBase = ref(GREEN_BASE)
 
 /**
- * Generates coordinates for a zone based on its position on the board
- * @param {boolean} moveRight - If true, shifts the zone 10 columns to the right
- * @param {boolean} moveDown - If true, shifts the zone 10 rows down
- * @returns {Array<Array<number>>} Array of [row, col] coordinates representing the zone positions
- */
-const zoneGenerator = (moveRight, moveDown) => {
-  const zone = []
-  for (let r = 1; r <= ZONE_SIZE; r++) {
-    for (let c = 1; c <= ZONE_SIZE; c++) {
-      const row = r + (moveDown ? 10 : 0)
-      const col = c + (moveRight ? 10 : 0)
-      zone.push([row, col])
-    }
-  }
-  return zone
-}
-
-const redZone = ref(zoneGenerator(false, false))
-const blueZone = ref(zoneGenerator(true, false))
-const yellowZone = ref(zoneGenerator(true, true))
-const greenZone = ref(zoneGenerator(false, true))
-
-/**
- * Checks if a given row and column match a zone or coordinate
- * @param {Array<Array<number>> | Array<number>} zone - Either an array of [row, col] coordinates or a single [row, col] coordinate
+ * Checks if a given row and column match any coordinate in a zone
+ * @param {Array<Array<number>>} zone - Array of [row, col] coordinates
  * @param {number} row - The row to check
  * @param {number} col - The column to check
- * @returns {boolean} True if the row and column match the zone or coordinate
+ * @returns {boolean} True if the row and column match any coordinate in the zone
  */
-const isZone = (zone, row, col) => {
-  if (!Array.isArray(zone[0])) {
-    // Handle single coordinate case
-    return zone[0] === row && zone[1] === col
-  }
-  // Handle array of coordinates case
+const onZone = (zone, row, col) => {
   return zone.some(([r, c]) => r === row && c === col)
 }
 
 const path = findPath([1, 7])
 </script>
+
 <template>
   <div>
     <div v-for="row in BOARD_SIZE" :key="row" class="row">
-      <div
+      <BoardCell
         v-for="col in BOARD_SIZE"
-        :key="col"
-        class="col"
-        :class="{
-          'red-zone': isZone(redZone, row, col),
-          'blue-zone': isZone(blueZone, row, col),
-          'yellow-zone': isZone(yellowZone, row, col),
-          'green-zone': isZone(greenZone, row, col),
-          'path-zone': isZone(path, row, col),
+        :key="(row - 1) * BOARD_SIZE + col"
+        :row="row"
+        :col="col"
+        :classes="{
+          'red-base': onZone(redBase, row, col),
+          'blue-base': onZone(blueBase, row, col),
+          'yellow-base': onZone(yellowBase, row, col),
+          'green-base': onZone(greenBase, row, col),
+          'path-square': onZone(path, row, col),
         }"
-      >
-        {{ row + ', ' + col }}
-      </div>
+      />
     </div>
   </div>
 </template>
+
 <style scoped>
 .row {
   display: flex;
 }
-.col {
-  border: solid black 1px;
-  color: black;
-  font-size: xx-small;
-  width: 40px;
-  height: 40px;
-}
-.red-zone {
+
+.red-base {
   background: red;
 }
-.blue-zone {
+
+.blue-base {
   background: blue;
 }
-.yellow-zone {
+
+.yellow-base {
   background: yellow;
 }
-.green-zone {
+
+.green-base {
   background: green;
 }
-.path-zone {
+
+.path-square {
   background: whitesmoke;
 }
 </style>
